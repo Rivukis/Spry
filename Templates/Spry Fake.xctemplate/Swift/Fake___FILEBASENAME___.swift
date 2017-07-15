386cd08418f93adc9c489781703e5d88c10e6728
jake <jake@geocaching.com>
2017-07-14 18:57:05 -0700

    Added Spry Fake template for Xcode.

---
+++ Templates/Spry Fake.xctemplate/Swift/Fake___FILEBASENAME___.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+import Spry
+
+@testable import ___PROJECTNAME___
+
+class Fake___VARIABLE_spryClass:identifier___: ___VARIABLE_spryClass:identifier___, Spryable {
+    var _spry: (calls: [RecordedCall], stubs: [Stub]) = ([], [])
+
+    enum Method {
+    }
+
+    //MARK: - overrides -
+
+    
+
+}
